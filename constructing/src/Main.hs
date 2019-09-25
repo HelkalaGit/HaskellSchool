ea8ad89ca4507e89478e8f345e3fc8b0de18f560
HelkalaGit <joel.helkala@gmail.com>
2019-09-25 14:42:24 +0300

    updated constr

--- constructing/src/Main.hs
+++ constructing/src/Main.hs
@@ -12,17 +12,26 @@ f3 (a,(b,c,d),e) = case (a,(b,c,d),e) of
 
 f4 :: [a] -> a
 f4 lst = case lst of
-   (x:xs) -> x
+   (x:xs) -> x  -- From the course documentation
 
 f5 :: Either Int String -> String
 f5 att = case att of 
          Left a -> "Not a string"
          Right b -> b   -- input should be f5 (Right "something")
+                        -- or              f5 (Left 5)
 
---f6 :: Either a b -> ? b
---g1 :: Maybe a -> b -> ? (Either a b)
---g2 :: a -> b -> ? (a,b)
---g3 :: a -> b -> ? (Either a b)
+f6 :: Either a b -> Maybe b
+f6 att = do Right x <- return att
+            return x -- Couldnt figure this one out by myself so looked from the                        stackoverflow.com 
+
+--g1 :: Maybe a -> b -> (Either a b)
+--g1 a b =  
+
+g2 :: a -> b ->  (a,b)
+g2 a b = (a,b) 
+
+--g3 :: a -> b -> Maybe (Either a b)
+--g3 a b =  
 
 main :: IO ()
 main = undefined
