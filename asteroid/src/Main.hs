7ca40855151c89df5da21bd1be5158d9cbef06d9
HelkalaGit <joel.helkala@gmail.com>
2019-10-23 13:11:13 +0300

    finished asteroid

--- asteroid/src/Main.hs
+++ asteroid/src/Main.hs
@@ -1,4 +1,7 @@
+{-#OPTIONS_GHC -fwarn-incomplete-patterns #-}
+
 module Main where
+import Debug.Trace
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 import Graphics.Gloss.Interface.Pure.Simulate
@@ -8,10 +11,25 @@ data AsteroidWorld = Play [Rock] Ship [Bullet] [Ufo]
                    | GameOver 
                    deriving (Eq,Show)
 
-
 type Velocity     = (Float, Float)
 type Size         = Float
 type Age          = Float
+type Health       = Float
+
+--newtype Hunt = Hunt Size Velocity Health
+--              deriving (Eq,Show)
+--newtype Flee = Flee Size Velocity Health
+--              deriving (Eq,Show)
+--newtype Explode = Explode Size
+--              deriving (Eq,Show)
+
+-- These types should contain atleast some sort of data, for example hp
+-- Possibly also color and size
+
+data UfoState     = Hunt Size Velocity Health 
+                  | Flee Size Velocity Health
+                  | Explode Size
+                   deriving (Eq,Show)
 
 
 data Ship   = Ship   PointInSpace Velocity      
@@ -20,8 +38,17 @@ data Bullet = Bullet PointInSpace Velocity Age
     deriving (Eq,Show)
 data Rock   = Rock   PointInSpace Size Velocity 
     deriving (Eq,Show)
-data Ufo    = Ufo    PointInSpace Size Velocity
+data Ufo    = Ufo    PointInSpace UfoState
     deriving (Eq,Show)
+
+--data Ufo    = Ufo    PointInSpace Size Velocity Health UfoState 
+--    deriving (Eq,Show)
+
+huntMode    = Hunt 50 (5,10) 3
+fleeMode    = Flee 30 (20,40) 2
+explodeMode = Explode 70
+
+-- mode for ufoStates
 
 initialWorld :: AsteroidWorld
 initialWorld = Play
@@ -33,33 +60,49 @@ initialWorld = Play
                    ] -- The default rocks
                    (Ship (0,0) (0,5)) -- The initial ship
                    [] -- The initial bullets (none)
-                   [Ufo (50,120)   20 (30,50)
-                   ,Ufo (-100,150)   20 (3,4)] -- The Ufos
+                   [Ufo (100,120) huntMode
+                   ,Ufo (-100,150) huntMode]
+                    -- The Ufos
+
 
 simulateWorld :: Float -> (AsteroidWorld -> AsteroidWorld)
 
 simulateWorld _        GameOver          = GameOver  
 
 simulateWorld timeStep (Play rocks (Ship shipPos shipV) bullets ufos) 
-  | any (collidesWith shipPos) rocks = GameOver
+  | any (collidesWithRock shipPos) rocks = GameOver
+  | any (collidesWithUfo shipPos) ufos = GameOver
   | otherwise = Play (concatMap updateRock rocks) 
                               (Ship newShipPos shipV)
                               (concat (map updateBullet bullets))
                               (concatMap updateUfo ufos) -- Ufo simulation
   where
-      collidesWith :: PointInSpace -> Rock -> Bool
-      collidesWith p (Rock rp s _) 
-       = magV (rp .- p) < s 
+      ufoSize t = case t of 
+       Hunt s _ _ -> s
+       Flee s _ _ -> s
+       Explode s  -> s
 
-      collidesWithBullet :: Rock -> Bool
-      collidesWithBullet r 
-       = any (\(Bullet bp _ _) -> collidesWith bp r) bullets 
-     
+      collidesWithRock :: PointInSpace -> Rock -> Bool
+      collidesWithRock p (Rock rp s _) 
+       = magV (rp .- p) < s
+
+      collidesWithUfo :: PointInSpace -> Ufo -> Bool
+      collidesWithUfo p (Ufo up t) 
+       = magV (up .- p) < ufoSize(t)  -- returns a boolean based on hit
+
+      collidesWithBulletRock :: Rock -> Bool
+      collidesWithBulletRock r 
+       = any (\(Bullet bp _ _) -> collidesWithRock bp r) bullets 
+    
+      collidesWithBulletUfo :: Ufo -> Bool
+      collidesWithBulletUfo r
+       = any (\(Bullet bp _ _) -> collidesWithUfo bp r) bullets -- returns boolean based on if bullet                                                                   hit ufo
+ 
       updateRock :: Rock -> [Rock]
       updateRock r@(Rock p s v) 
-       | collidesWithBullet r && s < 7 
+       | collidesWithBulletRock r && s < 7 
             = []
-       | collidesWithBullet r && s > 7 
+       | collidesWithBulletRock r && s > 7 
             = splitRock r
        | otherwise                     
             = [Rock (restoreToScreen (p .+ timeStep .* v)) s v]
@@ -68,22 +111,40 @@ simulateWorld timeStep (Play rocks (Ship shipPos shipV) bullets ufos)
       updateBullet (Bullet p v a) 
         | a > 5                      
              = []
-        | any (collidesWith p) rocks 
+        | any (collidesWithRock p) rocks 
              = [] 
+        | any (collidesWithUfo p) ufos
+             = []
         | otherwise                  
              = [Bullet (restoreToScreen (p .+ timeStep .* v)) v 
                        (a + timeStep)] 
 
       updateUfo :: Ufo -> [Ufo]
-      updateUfo r@(Ufo p s v)
-             = [Ufo (restoreToScreen (p .+ timeStep .* v)) s v] -- Ufo update
+      updateUfo r@(Ufo p (Explode _)) = []
 
+      updateUfo r@(Ufo p (Hunt _ v h))  
+        | collidesWithBulletUfo r && h > 0
+             = reduceHpUfo r
+        | otherwise
+            = traceShow h [Ufo (restoreToScreen (p .+ timeStep .* v)) huntMode] -- Ufo update
+        
+      updateUfo r@(Ufo p (Flee _ v h))  
+        | collidesWithBulletUfo r && h > 0
+            = reduceHpUfo r 
+        | otherwise
+            = traceShow h [Ufo (restoreToScreen (p .+ timeStep .* v)) fleeMode]
+ 
       newShipPos :: PointInSpace
       newShipPos = restoreToScreen (shipPos .+ timeStep .* shipV)
 
 splitRock :: Rock -> [Rock]
 splitRock (Rock p s v) = [Rock p (s/2) (3 .* rotateV (pi/3)  v)
                          ,Rock p (s/2) (3 .* rotateV (-pi/3) v) ]
+
+reduceHpUfo :: Ufo -> [Ufo]
+ 
+reduceHpUfo (Ufo p (Flee s v h)) = [Ufo p explodeMode]
+reduceHpUfo (Ufo p (Hunt s v h)) = [Ufo p fleeMode]
 
 restoreToScreen :: PointInSpace -> PointInSpace
 restoreToScreen (x,y) = (cycleCoordinates x, cycleCoordinates y)
@@ -107,13 +168,22 @@ drawWorld GameOver
 drawWorld (Play rocks (Ship (x,y) (vx,vy)) bullets ufos)
   = pictures [ship, asteroids, shots, ufo]
    where 
+    ufoColor h = case h of 
+      Hunt _ _ _ -> yellow
+      Flee _ _ _ -> green
+      Explode _ -> red
+    ufoSize s  = case s of
+      Hunt s _ _ -> s
+      Flee s _ _ -> s
+      Explode s  -> s
+
     ship      = color red (pictures [translate x y (circle 10)])
     asteroids = pictures [translate x y (color orange (circleSolid s)) 
-                         | Rock   (x,y) s _ <- rocks]
+                         | Rock   (x,y) s _  <- rocks]
     shots     = pictures [translate x y (color red (circle 2)) 
-                         | Bullet (x,y) _ _ <- bullets]
-    ufo       = pictures [translate x y (color green (circleSolid s))
-                         | Ufo    (x,y) s _ <- ufos] -- draw Ufo
+                         | Bullet (x,y) _ _  <- bullets]
+    ufo       = pictures [translate x y (color (ufoColor huntMode) (circleSolid (ufoSize huntMode)))
+                         | Ufo    (x,y) huntMode <- ufos] -- draw Ufo
 
 handleEvents :: Event -> AsteroidWorld -> AsteroidWorld
 
@@ -132,8 +202,6 @@ handleEvents (EventKey (MouseButton LeftButton) Down _ clickPos)
 
 handleEvents _ w = w
 
---restartGame :: AsteroidWorld
---restartGame Restart = playGame
 
 type PointInSpace = (Float, Float)
 (.-) , (.+) :: PointInSpace -> PointInSpace -> PointInSpace
@@ -170,13 +238,5 @@ playGame =
   handleEvents
   simulateWorld
 
-main = -- play 
-        -- (InWindow "Asteroids!" (550,550) (20,20)) 
-        -- black 
-        -- 24 
-        -- initialWorld 
-        -- drawWorld 
-        -- handleEvents
-        -- simulateWorld
-        playGame
+main = playGame
 
